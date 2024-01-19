b58c54f8d2e6943aa02b269714bf05e7c32353b8
coder-rupak <rupaksarkar005@gmail.com>
2024-01-20 00:35:49 +0530

    steeper incorporated

--- src/pages/Forms/Form1.tsx
+++ src/pages/Forms/Form1.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { Autocomplete, Grid, TextField } from "@mui/material";
+import {
+  Autocomplete,
+  Button,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
@@ -16,128 +22,143 @@ const govtIdTypeList = [
 
 interface FormInput {
   name: string;
-  dob: string;
-  mobile: string;
-  sex: string;
-  govtType: string;
-  govtId: string;
+  // dob: string;
+  // mobile: string;
+  // sex: string;
+  // govtType: string;
+  // govtId: string;
 }
+
+type Props = {
+  onContinue: any;
+};
 
 // yup schema
 const schema = yup.object().shape({
-  name: yup.string().required("Name required").min(3, "Min 3 characters"),
-  dob: yup.string().required("DOB required"),
-  mobile: yup
+  name: yup
     .string()
-    .required("Phone number required")
-    .matches(
-      /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/,
-      "Please enter valid phone number"
-    ),
-  sex: yup.string().required("State is a required field"),
-  govtType: yup.string().required("Phone is a required field"),
-  govtId: yup.string().required("Zipcode is a required field"),
+    .required("Name required")
+    .min(3, "Min 3 character required"),
+  // dob: yup.string().required("DOB required"),
+  // mobile: yup
+  //   .string()
+  //   .required("Phone number required")
+  //   .matches(
+  //     /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/,
+  //     "Please enter valid phone number"
+  //   ),
+  // sex: yup.string().required("Please select sex"),
+  // // govtType: yup.string().required("Phone is a required field"),
+  // govtId:
+  //   yup.string().required("Please type 12 digit Aadhar number").min(12) ||
+  //   yup.string().required("Please type 10 digit Pan number").min(10),
 });
 
-export const Form1: React.FC = () => {
+export const Form1 = ({ onContinue }: Props) => {
   const {
     register,
     handleSubmit,
-    // formState: { errors },
+    formState: { errors },
   } = useForm<FormInput>({ resolver: yupResolver(schema) });
 
   const onSubmit = (data: FormInput) => {
+    console.log("SUBMIT HIT");
+    onContinue();
     console.log(data);
   };
 
   const firstFormMarkup = (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Grid
-        container
-        direction="column"
-        justifyContent="center"
-        alignItems="stretch"
-        spacing={2}
-      >
-        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-          <TextField
-            required
-            id="outlined-required"
-            label="Name"
-            placeholder="Enter Name"
-            size="medium"
-            fullWidth
-            {...register("name")}
-            // error={errors?.name?.type === "required" ? true : false}
-          />
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Grid
+          container
+          direction="column"
+          justifyContent="center"
+          alignItems="stretch"
+          spacing={2}
+        >
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+            <TextField
+              id="name"
+              label="Name"
+              placeholder="Enter Name"
+              size="medium"
+              fullWidth
+              {...register("name")}
+            />
+            <Typography color="red" sx={{ pt: 1 }}>
+              {errors.name && errors?.name?.message}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+            <TextField
+              id="outlined-required"
+              label="Age"
+              placeholder="Age in Years"
+              size="medium"
+              fullWidth
+              // {...register("dob")}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+            <TextField
+              id="outlined-required"
+              label="Mobile"
+              placeholder="Enter Mobile"
+              size="medium"
+              fullWidth
+              // {...register("mobile")}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+            <Autocomplete
+              id="combo-box-demo"
+              options={sexList}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Sex"
+                  size="medium"
+                  fullWidth
+                  // {...register("sex")}
+                />
+              )}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+            <Autocomplete
+              id="combo-box-demo"
+              options={govtIdTypeList}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Govt Id Type"
+                  size="medium"
+                  fullWidth
+                  // {...register("govtType")}
+                />
+              )}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+            <TextField
+              id="outlined-required"
+              label="Enter Govt Id"
+              placeholder="Enter Govt Id"
+              size="medium"
+              fullWidth
+              // {...register("govtId")}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+            <Button variant="contained" fullWidth size="medium" type="submit">
+              Continue
+            </Button>
+            {/* <input type="submit" /> */}
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-          <TextField
-            required
-            id="outlined-required"
-            label="Age"
-            placeholder="Age in Years"
-            size="medium"
-            fullWidth
-            {...register("dob")}
-          />
-        </Grid>
-        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-          <TextField
-            required
-            id="outlined-required"
-            label="Mobile"
-            placeholder="Enter Mobile"
-            size="medium"
-            fullWidth
-            {...register("mobile")}
-          />
-        </Grid>
-        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-          <Autocomplete
-            id="combo-box-demo"
-            options={sexList}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label="Sex"
-                size="medium"
-                fullWidth
-                required
-                {...register("sex")}
-              />
-            )}
-          />
-        </Grid>
-        <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-          <Autocomplete
-            id="combo-box-demo"
-            options={govtIdTypeList}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label="Govt Id Type"
-                size="medium"
-                fullWidth
-                required
-                {...register("govtType")}
-              />
-            )}
-          />
-        </Grid>
-        <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-          <TextField
-            required
-            id="outlined-required"
-            label="Enter Govt Id"
-            placeholder="Enter Govt Id"
-            size="medium"
-            fullWidth
-            {...register("govtId")}
-          />
-        </Grid>
-      </Grid>
-    </form>
+      </form>
+    </>
   );
   return <>{firstFormMarkup}</>;
 };
--- src/pages/LoginLayout/index.tsx
+++ src/pages/LoginLayout/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import OnitoLogo from "../../assets/onito.png";
 import {
   Box,
-  Button,
   Card,
   CardContent,
   Grid,
@@ -11,36 +10,31 @@ import {
 } from "@mui/material";
 import { Form1 } from "../Forms/Form1";
 import { Form2 } from "../Forms/Form2";
-
-const steps = [
-  {
-    header: "Personal Details",
-    layout: <Form1 />,
-  },
-  {
-    header: "Address Detailes",
-    layout: <Form2 />,
-  },
-];
+import "./stepper.css";
 
 export default function Login() {
   const [activeStep, setActiveStep] = React.useState(0);
-  console.log("activeStep", activeStep);
-  const maxSteps = steps.length;
-
   const handleNext = () => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
   };
 
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
+  const steps = [
+    {
+      header: "Personal Details",
+      layout: <Form1 onContinue={handleNext} />,
+    },
+    {
+      header: "Address Details",
+      layout: <Form2 />,
+    },
+  ];
 
   const pageMarkup = (
     <Grid container direction="row" justifyContent="center" alignItems="center">
       <Card sx={{ minWidth: 500 }}>
         <img src={OnitoLogo} alt="Logo" height="80" />
         <CardContent>
+          {/* <Form1 onContinue={handleNext}/> */}
           <Box py={1}>
             <Typography fontWeight="bold">
               {steps[activeStep].header}
@@ -48,28 +42,12 @@ export default function Login() {
           </Box>
           {steps[activeStep].layout}
           <MobileStepper
-            variant="text"
-            steps={maxSteps}
+            variant="dots"
+            steps={steps.length}
             position="static"
             activeStep={activeStep}
-            nextButton={
-              <Button
-                size="small"
-                onClick={handleNext}
-                disabled={activeStep === maxSteps - 1}
-              >
-                Next
-              </Button>
-            }
-            backButton={
-              <Button
-                size="small"
-                onClick={handleBack}
-                disabled={activeStep === 0}
-              >
-                Back
-              </Button>
-            }
+            nextButton={null}
+            backButton={null}
           />
         </CardContent>
       </Card>
